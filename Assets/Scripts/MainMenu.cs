bd5075f274f314ee4668fef9173d4b5cadad7aa9
A.R <richardadrien0@gmail.com>
2019-03-15 23:49:24 +0100

    scripts ans scenes

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button QuitButton;
+    public Button PlayButton;
+
+    private void Start()
+    {
+        if (PlayButton == null)
+        {
+            PlayButton = GameObject.Find("PlayButton").GetComponent<Button>();
+            PlayButton.onClick.AddListener(OnPlayButtonClicked);
+        }
+        if (QuitButton == null)
+        {
+            QuitButton = GameObject.Find("QuitButton").GetComponent<Button>();
+            QuitButton.onClick.AddListener(OnQuitButtonClicked);
+        }
+    }
+
+    private void OnPlayButtonClicked()
+    {
+        Debug.Log("Quit Button clicked");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("appartement");
+    }
+
+    private void OnQuitButtonClicked()
+    {
+        Debug.Log("Quit Button clicked");
+        Application.Quit();
+    }
+}
