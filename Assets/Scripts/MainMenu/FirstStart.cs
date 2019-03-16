0b10709eb7f634ce6ad36d67bb073c5be9bab91f
A.R <richardadrien0@gmail.com>
2019-03-16 02:44:20 +0100

    mods

--- Assets/Scripts/MainMenu/FirstStart.cs
+++ Assets/Scripts/MainMenu/FirstStart.cs
@@ -1,18 +1,38 @@
-﻿using System.Collections;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+
+public class Account
+{
+    public string Email { get; set; }
+    public bool Active { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public IList<string> Roles { get; set; }
+}
 
 public class FirstStart : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        string json = @"{
+  'Email': 'james@example.com',
+  'Active': true,
+  'CreatedDate': '2013-01-20T00:00:00Z',
+  'Roles': [
+    'User',
+    'Admin'
+  ]
+}";
+
+        Account account = JsonConvert.DeserializeObject<Account>(json);
+
+        Debug.Log(account.Email);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
     }
 }
