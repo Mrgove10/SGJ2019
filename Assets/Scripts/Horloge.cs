2f3dd209a652b82eff44f87ff50a61c92e69d3db
Axsiow <tehsiow2@gmail.com>
2019-03-16 01:11:37 +0100

    [fix] horloge

--- Assets/Scripts/Horloge.cs
+++ Assets/Scripts/Horloge.cs
@@ -1,32 +1,31 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Horloge : MonoBehaviour
-{
+public class Horloge : MonoBehaviour{
     public int heure = 0;
 
     public int minute = 0;
     public string horlogue = "";
+    public Text horloguetext;
 
     // Start is called before the first frame update
-    private void Start()
-    {
+    private void Start(){
         StartCoroutine(ajout());
     }
 
     // Update is called once per frame
-    private void FixedUpdate()
-    {
+    private void FixedUpdate(){
         horlogue = heure + ":" + minute;
-        Debug.Log(horlogue);
+        horloguetext.text = (heure + " minute");
     }
 
-    private IEnumerator ajout()
-    {
-        while (true)
-        {
+    private IEnumerator ajout(){
+        while (true){
             yield return new WaitForSeconds(1);
             minute++;
+            Debug.Log(horlogue);
         }
     }
 }
