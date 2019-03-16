c4981dbabdd96dd4cc523f5e1b80b6816b806ce0
Théo <theo_38180@live.fr>
2019-03-16 01:10:19 +0100

    [add] menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,19 +5,56 @@ public class MainMenu : MonoBehaviour
 {
     public Button PlayButton;
     public Button QuitButton;
+    public Toggle ToggleM;
+    public Toggle ToggleF;
+    public InputField InputNom;
+    public bool isPlayPossible;
+
 
     private void Start()
     {
+
         if (PlayButton == null)
         {
             PlayButton = GameObject.Find("PlayButton").GetComponent<Button>();
             PlayButton.onClick.AddListener(OnPlayButtonClicked);
         }
+
+        PlayButton.interactable = false;
+
+        if (InputNom == null)
+        {
+            InputNom = GameObject.Find("InputNom").GetComponent<InputField>();
+           // PlayButton.onClick.AddListener(OnPlayButtonClicked);
+        }
+
+        if (ToggleM == null)
+        {
+            ToggleM = GameObject.Find("ToggleM").GetComponent<Toggle>();
+            //PlayButton.onClick.AddListener(OnPlayButtonClicked);
+        }
+
+        if (ToggleF == null)
+        {
+            ToggleF = GameObject.Find("ToggleF").GetComponent<Toggle>();
+            //PlayButton.onClick.AddListener(OnPlayButtonClicked);
+        }
+
         if (QuitButton == null)
         {
             QuitButton = GameObject.Find("QuitButton").GetComponent<Button>();
             QuitButton.onClick.AddListener(OnQuitButtonClicked);
         }
+    }
+
+    public void FixedUpdate()
+    {
+        if ((ToggleM.isOn || ToggleF.isOn) && InputNom.text != "")
+        {
+            Debug.Log("TOUT VAS BIEN OUI AAAAAAAA OUI JE PEUX CLIQUER SUR LE BOUTON ASKIP");
+            PlayButton.interactable = true;
+        }
+        else { PlayButton.interactable = false; }
     }
 
     private void OnPlayButtonClicked()
