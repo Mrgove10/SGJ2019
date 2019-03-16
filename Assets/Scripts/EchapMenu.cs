576c04fc69ac268a67c4b13aac3c6b896abbd206
Axsiow <tehsiow2@gmail.com>
2019-03-16 04:09:57 +0100

    [add] EchapMenu

--- Assets/Scripts/EchapMenu.cs
+++ Assets/Scripts/EchapMenu.cs
@@ -1,19 +1,54 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+public class EchapMenu : MonoBehaviour
+{
+    public bool IsEscapeMenuPossible = true;
 
-public class EchapMenu : MonoBehaviour {
+    public bool MenuActive = false;
+
+    public GameObject EscapeMenu;
+
+    public Button QuitButtonESC;
+
     // Start is called before the first frame update
-    void Start() {
-        
+    private void Start()
+    {
+        EscapeMenu = GameObject.Find("EscapeMenu");
+        EscapeMenu.SetActive(false);
+        if (QuitButtonESC == null)
+        {
+            QuitButtonESC = GameObject.Find("QuitButtonESC").GetComponent<Button>();
+            QuitButtonESC.onClick.AddListener(OnQuitButtonESCClicked);
+        }
     }
 
     // Update is called once per frame
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.Escape)) {
-            Debug.Log("Touche esc appyé !!");
-        }
+    private void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            Debug.Log("Escape Key Pressed");
+            if (MenuActive == true)
+            {
+                EscapeMenu.SetActive(false);
+                MenuActive = false;
+            }
+            else
+            {
+                if (IsEscapeMenuPossible == true)
+                {
+                    MenuActive = true;
 
+                    EscapeMenu.SetActive(true);
+                }
+            }
+        }
+    }
+
+    void OnQuitButtonESCClicked()
+    {
+        Application.Quit();
     }
 }
